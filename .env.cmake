26ba34c4feb5136ab71eebd82db712f9fdbad699
Igor Boyarshin <igor.boyarshin@gmail.com>
2023-07-28 13:17:16 +0300

    Updated env variables

--- .env.cmake
+++ .env.cmake
@@ -1,10 +1,10 @@
-set(GLFW_PATH C:/work/util/GLFW)
-set(GLM_PATH C:/work/util/GLM)
-set(VULKAN_SDK_PATH C:/work/VulkanSDK/1.3.216.0)
+set(GLFW_PATH C:/Stuff/programs/glfw)
+set(GLM_PATH C:/Stuff/programs/glm)
+set(VULKAN_SDK_PATH C:/Stuff/programs/VulkanSDK-1.3.250.1)
 
 # Set MINGW_PATH if using mingwBuild.bat and not VisualStudio20XX
 # set(MINGW_PATH "C:/Program Files/mingw-w64/x86_64-8.1.0-win32-seh-rt_v6-rev0/mingw64")
-set(MINGW_PATH "C:/work/util/w64devkit")
+set(MINGW_PATH "C:/Stuff/programs/w64devkit")
 
 # Optional set TINYOBJ_PATH to target specific version, otherwise defaults to external/tinyobjloader
 # set(TINYOBJ_PATH X:/dev/Libraries/tinyobjloader)
